554c651905f7d7ec36aeb86202b340048ba025d3
Anonymus13 <jfcusicanqui@gmail.com>
2015-04-24 18:40:36 +0200

    Matrix inverted functions
    Assignment 2, inversion of matrix by cached values.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below perform the following steps:
+## makeCacheMatrix() creates a list to cache the matrix and its inverse
+## to use them in further operations.
+## cachesolve() verifies if there is a saved value for the inverted matrix and
+## return it, and where is not it computes and chaches it.
 
-## Write a short comment describing this function
+## This function first assign a null value to minv, then use a free variable 'y'
+## to set a values to the matrix x. In the same way uses solve function to
+## obtain the inverted matrix and stores it in "setminv". In parallel it gets 
+## and stores this values into "get" and "serinv" variables.
 
 makeCacheMatrix <- function(x = matrix()) {
+                minv <- NULL
+                set <- function(y) {
+                  x <<- y
+                  minv <<- NULL
+                }
+                get <- function() x
+                setminv <- function(solve) minv <<- solve
+                getminv <- function() minv
+                list(set = set, get = get,
+                     setminv = setminv,
+                     getminv = getminv)
+                }
 
-}
 
-
-## Write a short comment describing this function
+## This function retrieve and check if there is an stored value for "minv"
+## in the case it exists, it returns "getting cached data", however if not
+## it compute it by solve() function and stored in "serminv" variable.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                minv <- x$getminv()
+                if(!is.null(minv)) {
+                  message("getting cached data")
+                }
+                data <- x$get()
+                minv <- solve(data, ...)
+                x$setminv(minv)
+                minv
 }
